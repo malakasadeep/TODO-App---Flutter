3104c2e0acbf0435c53076069b8782c739f2052e
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-07-19 15:06:44 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:todo_app/main_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
