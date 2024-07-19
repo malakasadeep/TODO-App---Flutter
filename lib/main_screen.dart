e1076c9216942561c932a6208fa10ef954532b37
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-07-19 15:16:36 +0530

    chore: Update analysis_options.yaml and main_screen.dart

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -11,9 +13,18 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         centerTitle: true,
         title: Text("TODO App"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.add,
+            ),
+          ),
+        ],
       ),
     );
   }
